ed4a4d00531d1fd4cdc4f67e1353651f5b2eefff
Mateo Martinjak <mateo1559@gmail.com>
2019-07-31 00:14:00 +0200

    button

--- SFML_RPG/Button.h
+++ SFML_RPG/Button.h
@@ -1,8 +1,37 @@
 #pragma once
+
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
+#include<sstream>
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+
+enum button_states{BTN_IDLE=0, BTN_HOVER,BTN_ACTIVE};
 class Button
 {
+private:
+	
+	short unsigned buttonState;
+	sf::RectangleShape shape; //button basic shape
+	sf::Font* font;
+	sf::Text text;
+
+	sf::Color idleColor;
+	sf::Color hoverColor;
+	sf::Color activeColor;
+
 public:
-	Button();
+	Button(float x,float y, float width,float height,
+		sf::Font* font,std::string text,
+		sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
 	~Button();
+	//functions
+
+	void update(const sf::Vector2f mousePos);
+	void render(sf::RenderTarget* target);
 };
 
