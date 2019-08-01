163147b2b0223fdc53da65a18f52b66cd97fc104
Mateo Martinjak <mateo1559@gmail.com>
2019-08-01 20:47:30 +0200

    animation class almost completed

--- SFML_RPG/AnimationComponent.cpp
+++ SFML_RPG/AnimationComponent.cpp
@@ -1,12 +1,15 @@
 #include "AnimationComponent.h"
 
-
-
-AnimationComponent::AnimationComponent()
+AnimationComponent::AnimationComponent(sf::Sprite & sprite, sf::Texture & textureSheet)
+	:sprite(sprite),textureSheet(textureSheet)
 {
 }
 
-
 AnimationComponent::~AnimationComponent()
+{
+}
+
+//Functions
+void AnimationComponent::update(const float & dt)
 {
 }
--- SFML_RPG/AnimationComponent.h
+++ SFML_RPG/AnimationComponent.h
@@ -17,39 +17,73 @@ private:
 	{
 	public:
 		//Variables
+		sf::Sprite& sprite;
 		sf::Texture& textureSheet;
-		float speed;
+		float animationTimer;
 		int width;
 		int height;
+		float timer;
 		sf::IntRect startRect;
+		sf::IntRect currentRect;
 		sf::IntRect endRect;
 
-		Animation(sf::Texture& textureSheet,float speed,
+		Animation(sf::Sprite& sprite,sf::Texture& textureSheet,
+			float animationTimer,
 			int start_x, int start_y,
 			int end_x, int end_y,
 			int width,int  height)
-			:textureSheet(textureSheet),speed(speed),width(width),height(height)
+			:sprite(sprite),textureSheet(textureSheet), animationTimer(animationTimer),width(width),height(height)
 		{
 		
 
 			this->startRect = sf::IntRect(start_x, start_y, width, height);
+			this->currentRect = this->startRect;
 			this->endRect = sf::IntRect(end_x, end_y, width, height);
+
+			this->sprite.setTexture(this->textureSheet,true);//in case of error,set true?
+			this->sprite.setTextureRect(this->startRect);
 		}
 
 		//Functions
 		void update(const float& dt) 
 		{
+			this->timer = 10.f * dt;
+			//update timer
+			if (this->timer >= this->animationTimer)
+			{
+				//reset timer
+				this->timer = 0.f;
+				//Animate
+				if (this->currentRect != this->endRect)
+				{
+					this->currentRect.left += this->width;
+				}
+				else //Reset
+				{
+					this->currentRect.left = this->startRect.left;
+				}
 
+			}
 		}
 		void pause();
 		void reset();
 	};
 
+	sf::Sprite& sprite;
+	sf::Texture& textureSheet;
 	std::map<std::string, Animation> animations;
 public:
-	AnimationComponent();
+	AnimationComponent(sf::Sprite& sprite,sf::Texture& textureSheet);
 	virtual ~AnimationComponent();
 
 	//Functions
+	void addAnimation(const std::string key);
+
+	void startAnimation(const std::string animation);
+	void pauseAnimation(const std::string animation);
+	void resetAnimation(const std::string animation);
+
+	void update(const float& dt);
+
 };
 
